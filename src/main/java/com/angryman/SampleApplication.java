1ed1d1df8f4c1fc6e52a33bfa254806af9b6ca2b
AngryLuna <wsqa1231@gmail.com>
2020-07-30 17:34:37 +0900

    일반 어플리케이션 실행으로 변경 (히스토리용)

--- src/main/java/com/angryman/SampleApplication.java
+++ src/main/java/com/angryman/SampleApplication.java
@@ -1,11 +1,19 @@
 package com.angryman;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
 public class SampleApplication {
     public static void main(final String[] args) {
-        SpringApplication.run(SampleApplication.class);
+//        SpringApplication.run(SampleApplication.class, args);
+        final SpringApplication springApplication = new SpringApplication(SampleApplication.class);
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
+        springApplication.run(args);
     }
 }
