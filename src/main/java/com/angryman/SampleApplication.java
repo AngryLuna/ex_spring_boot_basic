bcb23bb1f3dd5efdae6d3b502a2d36fee1d12da9
AngryLuna <wsqa1231@gmail.com>
2020-07-30 18:26:17 +0900

    코드 복구

--- src/main/java/com/angryman/SampleApplication.java
+++ src/main/java/com/angryman/SampleApplication.java
@@ -1,11 +1,14 @@
 package com.angryman;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
     public static void main(final String[] args) {
-        SpringApplication.run(SampleApplication.class, args);
+        final SpringApplication springApplication = new SpringApplication();
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
+        springApplication.run(SampleApplication.class, args);
     }
 }
