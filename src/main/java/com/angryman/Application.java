396548eb7b91cfabf7ede961604cf65be363c5a9
AngryLuna <wsqa1231@gmail.com>
2020-08-12 14:43:05 +0900

    SSL 설정 / http(8080), https(443) 포트 설정

--- src/main/java/com/angryman/Application.java
+++ src/main/java/com/angryman/Application.java
@@ -1,10 +1,35 @@
 package com.angryman;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+
+    @Bean
+    public ServletWebServerFactory servletFactory() {
+        final TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        final Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
+
     public static void main(final String[] args) {
         SpringApplication.run(Application.class);
     }
