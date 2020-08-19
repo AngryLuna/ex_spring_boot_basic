6b57430298931edbfcd7d5b0d4b532bb65f71051
AngryLuna <wsqa1231@gmail.com>
2020-08-19 22:10:12 +0900

    application.properties 위치에 따른 우선순위 확인

--- src/main/java/com/angryman/SampleApplicationRunner.java
+++ src/main/java/com/angryman/SampleApplicationRunner.java
@@ -23,6 +23,11 @@ public class SampleApplicationRunner implements ApplicationRunner {
         System.out.println(String.format("foo : %b", args.containsOption("foo")));
         System.out.println(String.format("bar : %b", args.containsOption("bar")));
 
+        // application.properties는 같은 이름으로 4군데 둘 수 있으며 우선순위가 높게 설정된 값으로 덮어씀
+        // 1순위 : file:./config/application.properties
+        // 2순위 : file:./application.properties
+        // 3순위 : classpath:./config/application.properties
+        // 4순위 : classpath:./application.properties
         System.out.println(String.format("testName : %s", this.testName));
         System.out.println(String.format("testAge : %d", this.testAge));
         System.out.println(String.format("testFullName : %s", this.testFullName));
